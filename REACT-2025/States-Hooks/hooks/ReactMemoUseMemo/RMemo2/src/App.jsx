450bcdc166bf569ab3e2921cbb6cc17aeeafdd51
Nasim <nasim.reja.077@gmail.com>
2025-10-09 18:18:13 +0530

    Explen React.Memo() And useMemo Working

---
+++ REACT-2025/States-Hooks/hooks/ReactMemoUseMemo/RMemo2/src/App.jsx
@@ -0,0 +1,12 @@
+import { ReactMemo2 } from "./component/ReactMemo"
+
+
+export const App = () => {
+  return (
+    <>
+    <div className="flex flex-column e-justify-center items-center">
+      <ReactMemo2 />
+    </div>
+    </>
+  )
+}
