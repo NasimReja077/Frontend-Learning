846457948aeba942397601d5dd3474c9293faeea
Nasim <nasim.reja.077@gmail.com>
2025-10-12 18:24:19 +0530

    Create Global Loading States in React Using React Router & useNavigation

--- REACT-2025/React-Router-V6.4/react-route1/src/component/layout/AppLayout.jsx
+++ REACT-2025/React-Router-V6.4/react-route1/src/component/layout/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Header } from './Header'
 import { Lodding } from './Loading'
 // Create Global Loading States in React Using React Router & useNavigation
 const AppLayout =()=> {
-  const navigation = useNavigation() //useNavigate is defarent 
+  const navigation = useNavigation() //useNavigate is defarent //useNavigate is funtion
   console.log(navigation);
   if (navigation.state === "loading") return <Lodding />
   return (
