563f6300056a6e30438ef9c455c160a595797402
Nasim <nasim.reja.077@gmail.com>
2025-10-12 18:18:44 +0530

    Fetch API Data and Add .env.VITE_API_KEY

---
+++ REACT-2025/React-Router-V6.4/react-route1/src/api/GatApiData.jsx
@@ -0,0 +1,12 @@
+export const getMoviesData = async () => {
+     try {
+          const response = await fetch(
+               // "http://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1"
+               `http://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`
+          );
+          const data = response.json();
+          return data;
+     } catch (error) {
+          console.log(error)
+     }
+}
