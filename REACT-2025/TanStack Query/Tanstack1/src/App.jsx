9bfc277ea1918081938bcc01665296037ea6a827
Nasim <nasim.reja.077@gmail.com>
2025-10-27 13:16:04 +0530

    Add QueryClientProvider, QueryClient

--- REACT-2025/TanStack Query/Tanstack1/src/App.jsx
+++ REACT-2025/TanStack Query/Tanstack1/src/App.jsx
@@ -1,8 +1,59 @@
+/**
+ * What is React Query (TanStack Query)?
+
+React Query helps you fetch, cache, and update data in React apps — without writing manual useEffect, useState, or fetch() logic everywhere.
+
+It manages:
+
+✅ Data fetching
+
+✅ Caching and background refetching
+
+✅ Loading and error states
+
+✅ Automatic updates (when data changes)
+
+⚙️ QueryClient
+
+The “brain” or manager” of React Query.
+
+It stores and controls all your queries, caching behavior, and background fetching logic.
+
+✅ Syntax:
+import { QueryClient } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+
+QueryClientProvider
+
+The “Context Provider” that makes the QueryClient available to your entire app.
+
+It’s like React’s Context.Provider — it gives access to React Query features (like useQuery, useMutation, etc.) to all components inside it.
+
+✅ Syntax:
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+const queryClient = new QueryClient();
+const Root = () => (
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
+export default Root;
+
+Now, any component inside <QueryClientProvider> can use hooks like:
+useQuery()
+useMutation()
+useQueryClient()
+
+ */
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MainLayout } from "./component/Layouts/MainLayout";
 import { Home } from "./pages/Home";
 import { FetchOld } from "./pages/FetchOld";
 import { FetchRQ } from "./pages/FetchRQ";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
@@ -26,7 +77,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = ()=>{
-  return <RouterProvider router={router}></RouterProvider>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>;
+    </QueryClientProvider>
+  )
 };
 
 export default App
