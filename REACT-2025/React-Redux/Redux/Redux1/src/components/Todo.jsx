d4918556d3b8e0665db2b4794072a742ceba27be
Nasim <nasim.reja.077@gmail.com>
2025-10-30 22:46:41 +0530

    Initialize React , Redux Todo application with essential components and configurations - Created Redux store with actions for adding and deleting tasks. - Integrated Redux DevTools

---
+++ REACT-2025/React-Redux/Redux/Redux1/src/components/Todo.jsx
@@ -0,0 +1,54 @@
+import { useDispatch, useSelector } from "react-redux";
+import { MdDeleteForever } from "react-icons/md";
+import { useState } from "react";
+import { addTasks, deleteTasks } from "../store";
+
+export const Todo = () => {
+     const [task, setTask] = useState("")
+
+     const tasks = useSelector((state) => state.task); //Access Redux State in React , Read Data from the Redux Store using useSelector
+     // console.log("react states", state.task);
+
+     // Dispatch Actions in React using Redux: Add and Delete Tasks with useDispatch
+     const dispatch= useDispatch();
+
+     // handle Form Submit
+     const handleFormSubmit = (e) => {
+          e.preventDefault();
+          dispatch(addTasks(task));
+          return setTask("");
+     }
+
+     // handle Task Delete
+     const handleTaskDelete = (id) =>{
+          return dispatch(deleteTasks(id));
+     };
+
+     return (
+          <div className="container flex item-center bg-pink-500">
+               <div>
+                    <h1>ToDo-List</h1>
+                    <div>
+                         <form onSubmit={handleFormSubmit}>
+                              <input type="text" id="input-box" placeholder="Add a new Task" value={task} onChange={(e) => setTask(e.target.value)}/>
+                              <button>Add Task</button>
+                         </form>
+                    </div>
+                    <ul>
+                         {tasks.map((curTask, index) => {
+                              return (
+                                   <li key={index}>
+                                        <p>
+                                             {index}:{curTask}
+                                        </p>
+                                        <MdDeleteForever 
+                                        onClick={() => handleTaskDelete(index)}
+                                        />
+                                   </li>
+                              );
+                         })}
+                    </ul>
+               </div>
+          </div>
+     );
+};
---
+++ REACT-2025/React-Redux/Redux/Redux1/src/store.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable no-case-declarations */
+// Create Reducer Functions to Add & delete
+import { createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+const Add_Task = "task/add";
+const Delete_Task = "task/delete"
+const initialState = {
+     // task=[] not use when use obj
+     task: [], // key valu paer
+     isLoading: false,
+};
+
+// Step 1: Create a simplereducer function
+const taskReducer = (state = initialState, action) =>{
+     switch (action.type){
+          case Add_Task:
+               return{
+                    ...state,
+                    task:[...state.task, action.payload],
+               };
+          case Delete_Task:
+               const updatedTask = state.task.filter((curTask, index) => {
+                    return index !== action.payload;
+               });
+               return{
+                    ...state,
+                    task: updatedTask,
+               };
+               default:
+                    return state;
+     }
+};
+
+// Redux Store: Create, Dispatch & Get State in React
+// Step 2: Create the Redux store using the reducer
+export const store = createStore(taskReducer, composeWithDevTools());
+console.log(store);
+
+// Step 3: Log the initial state
+// The getState method is a synchronous function that returns the current state of a Redux
+// application. It includes the entire state of the application, including all the reducers
+// and their respective states.
+console.log("initial State: ", store.getState());
+
+// Step 5: Create action creators
+export const addTasks = (data) =>{
+     return { type: Add_Task, payload: data};
+};
+
+export const deleteTasks = (id) =>{
+     return { type: Delete_Task, payload: id};
+};
+
+
+// Step 4: Dispatch an action to add a task 
+
+// store.dispatch({ type: Add_Task, payload: "Hi I am Redax" });
+store.dispatch( addTasks ("Hi I am Redax" ));
+console.log("updated State: ", store.getState());
+
+// store.dispatch({ type: Add_Task, payload: "MY Name Is Hero" });
+store.dispatch( addTasks ("MY Name Is Hero" ));
+console.log("updated State: ", store.getState());
+
+// store.dispatch({ type: Delete_Task, payload: 1});
+store.dispatch(deleteTasks(1));
+console.log("deleted State: ", store.getState());
+
+
+
