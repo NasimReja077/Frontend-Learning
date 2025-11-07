7289356aa92ad5b1bbbfaedba53d30a500f0bbd5
Nasim <nasim.reja.077@gmail.com>
2025-11-07 21:11:39 +0530

    Add Weather App with Mocked RTK Query

---
+++ REACT-2025/React-Redux/Redux-Toolkit-QUERY/Weather-Api/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import WeatherSearch from './components/WeatherSearch.js';
+import WeatherDisplay from './components/WeatherDisplay.js';
+
+import './App.css'
+
+const App = () => {
+  const [city, setCity] = useState('');
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch onSearch={setCity} />
+      <WeatherDisplay city={city} />
+    </div>
+  );
+};
+
+export default App;
