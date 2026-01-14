7256fddf721b9d46d70f958d71bf6eddf7f38640
Nasim <nasim.reja.077@gmail.com>
2026-01-14 23:03:01 +0530

    Add image popup files

---
+++ REACT-2025/File Uploading/Image Popup Modal/ImagePopup2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ProfileImage } from './components/ProfileImage'
+import { ProfileImage2 } from './components/ProfileImage2'
+
+const App = () => {
+  return (
+    <>
+    <ProfileImage/>
+    <br />
+    <ProfileImage2/>
+    </>
+  )
+}
+
+export default App
