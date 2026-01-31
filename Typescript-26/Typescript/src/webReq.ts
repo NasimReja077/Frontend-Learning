5aafb8470b50237794ea790eb286ed199448985c
Nasim <nasim.reja.077@gmail.com>
2026-01-31 23:53:48 +0530

    Add fetchData function and ToDo interface for Axios data retrieval example

---
+++ Typescript-26/Typescript/src/webReq.ts
@@ -0,0 +1,28 @@
+import axios,{AxiosResponse} from "axios"
+
+interface ToDo {
+     userId: number;
+     id: number;
+     title: string;
+     completed: boolean;
+}
+// axios.get("https://example.com/data")
+// .then(response => {
+//      console.log(response.data);
+// })
+
+const fetchData = async() => {
+     try {
+  const response: AxiosResponse<Todo> = await axios.
+  get(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+  console.log("Todo", response.data);
+
+} catch (error: any) {
+     if (axios.isAxiosError(error)){
+          console.log("Axios error message: ", error.message);
+     }
+}
+}
+
