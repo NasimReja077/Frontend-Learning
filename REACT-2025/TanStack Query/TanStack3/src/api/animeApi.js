9f8c76a7121a6dacf80cf6441d76c1115e474148
Nasim <nasim.reja.077@gmail.com>
2026-04-23 23:32:50 +0530

    feat: implement pagination for character fetching with useInfiniteQuery

--- REACT-2025/TanStack Query/TanStack3/src/api/animeApi.js
+++ REACT-2025/TanStack Query/TanStack3/src/api/animeApi.js
@@ -1,11 +1,27 @@
 import axios from 'axios'
+import { useInfiniteQuery } from '@tanstack/react-query'
 
 const BASE_URL = 'https://dragonball-api.com/api'
 
 // Fetch all characters with a limit of 52
-export const fetchCharacters = async () => {
-  const res = await axios.get(`${BASE_URL}/characters?limit=52`)
-  return res.data.items
+export const fetchCharacters = async ({ pageParam = 1 }) => {
+  const res = await axios.get(`${BASE_URL}/characters?page=${pageParam}&limit=12`)
+  return {
+    characters: res.data.items,
+    nextPage:
+      res.data.meta?.currentPage < res.data.meta?.totalPages
+        ? res.data.meta.currentPage + 1
+        : undefined,
+  }
+}
+
+export const useCharacters = () => {
+  return useInfiniteQuery({
+    queryKey: ['characters'],
+    queryFn: fetchCharacters,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  })
 }
 
 // Fetch a single character by ID
