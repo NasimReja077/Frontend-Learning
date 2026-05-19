84cd338c8d5ffdca079429b6312bec20a892da14
Nasim <nasim.reja.077@gmail.com>
2026-05-19 22:44:48 +0530

    feat: create App component to integrate ImageSlider with header

---
+++ Image-Slider/src/App.jsx
@@ -0,0 +1,15 @@
+import ImageSlider from './ImageSlider';
+
+ const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', margin: '2rem' }}>
+        DSA-Powered Image Slider
+      </h1>
+      <ImageSlider />
+    </div>
+  );
+}
+
+export default App;
+
